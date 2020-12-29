6b57f3df7e7535d80ebef44504abafa9b30bd0f9
woodchuckchoi <choiggoggi@gmail.com>
2020-12-30 02:17:04 +0900

    demo still in progress

--- msa/images/service_c/server.js
+++ msa/images/service_c/server.js
@@ -8,8 +8,11 @@ const HOST = "0.0.0.0";
 
 const app = express();
 app.get('/', (req, res) => {
-    let res_a = axios.get('http://service_a:7777');
-    let res_b = axios.get('http://service_b:8888');
+    const promise = (url) => {
+        return axios.get(url).then((res)=> res.data).catch((err)=>`Error from ${url}`)
+    }
+    let res_a = promise('http://service_a.default:7777');
+    let res_b = promise('http://service_b.default:8888');
     
     res.send(`Service C, version 1.0\n${res_a}\n${res_b}`);
 });
