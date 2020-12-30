441fdfb8ad07483a311b185eba598678de020578
woodchuckchoi <choiggoggi@gmail.com>
2020-12-30 10:57:20 +0900

    files moved around

--- msa/images/service_c/server.js
+++ msa/images/service_c/server.js
@@ -11,8 +11,13 @@ app.get('/', (req, res) => {
     const promise = (url) => {
         return axios.get(url).then((res)=> res.data).catch((err)=>`Error from ${url}`)
     }
-    let res_a = promise('http://service_a.default:7777');
-    let res_b = promise('http://service_b.default:8888');
+    let res_a, res_b;
+    promise('http://service_a.default:7777').then((data)=>{
+        res_a = data;
+    });
+    promise('http://service_b.default:8888').then((data)=>{
+        res_b = data;
+    });
     
     res.send(`Service C, version 1.0\n${res_a}\n${res_b}`);
 });
